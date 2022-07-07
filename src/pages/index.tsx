ea5fc031e420aaacedf2cab6c0e2dcc0312fc074
David Malmström <syntex@outlook.com>
2022-07-07 02:50:07 +0200

    call and get ids

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+  const hello = trpc.useQuery(["example.getAll"]);
 
   return (
     <>
@@ -78,7 +78,15 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+          {hello.data ? (
+            <p>
+              {hello.data.map((d) => (
+                <div>{d.id}</div>
+              ))}
+            </p>
+          ) : (
+            <p>Loading..</p>
+          )}
         </div>
       </div>
     </>
