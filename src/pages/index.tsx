2b6736841fb80dd8f7a5448f339bb0b20488a3ff
David Malmström <syntex@outlook.com>
2022-07-13 04:00:44 +0200

    fix lint problems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="pt-40  items-center flex-1 w-full h-full flex flex-col">
+    <div className="flex h-full w-full flex-1 flex-col items-center pt-40">
       <div className="text-[5rem] font-extrabold">Plan together</div>
     </div>
   )
